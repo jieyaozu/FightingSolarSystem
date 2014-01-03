ed3639a68e42fb6a8d7559c88a8b22ef89e6594f
unknown <DELL-PC@jieyaozu.(none)>
2014-01-03 10:23:31 +0800

    update file #

--- Classes/Resource.h
+++ Classes/Resource.h
@@ -10,6 +10,7 @@
 #define PlanGame_Resource_h
 
 // image 
+static const char s_bg[]              = "../Resources/background.png";
 static const char s_bg01[]              = "../Resources/bg01.jpg";
 static const char s_bg02[]              = "../Resources/bg02.jpg";
 static const char s_loading[]           = "../Resources/loading.png";
@@ -31,21 +32,31 @@ static const char s_arial14[]           = "../Resources/arial-14.png";
 static const char s_arial14_fnt[]       = "../Resources/arial-14.fnt";
 static const char s_pause[]             = "../Resources/pause.png";
 static const char s_play[]              = "../Resources/play.png";
-
+static const char s_target[]            = "../Resources/sign.png";
+static const char s_fire[]              = "../Resources/fire.png";
+static const char s_daodan[]            = "../Resources/daodan.png";
+static const char s_shadow[]            = "../Resources/ship_shadow.png";
+static const char s_boom[]              = "../Resources/boom.png";
+static const char s_boss_one[]          = "../Resources/boss_one.png";
+static const char s_boss_one_bomb[]     = "../Resources/boss_one_bomb.png";
+static const char s_boss_bullet[]       = "../Resources/boss_bullet.png";
 
 // font
 static const char s_font[]              = "../Resources/Font/bitmapFontTest.fnt";
 
 // music
 static const char s_bgMusic[]           = "../Resources/Music/bgMusic.wav";
-static const char s_mainMainMusic[]     = "../Resources/Music/mainMainMusic.mp3";
+static const char s_mainMainMusic[]     = "../Resources/Music/mainMainMusic.wav";
 
 // sound effect
-static const char s_buttonEffect[]      = "../Resources/Music/buttonEffet.mp3";
+static const char s_buttonEffect[]      = "../Resources/Music/buttonEffet.wav";
 static const char s_explodeEffect[]     = "../Resources/Music/explodeEffect.wav";
 static const char s_fireEffect[]        = "../Resources/Music/sounds0014.wav";
-static const char s_shipDestroyEffect[] = "../Resources/Music/shipDestroyEffect.mp3";
+static const char s_shipDestroyEffect[] = "../Resources/Music/shipDestroyEffect.wav";
 static const char s_moveout[]           = "../Resources/Music/moveout.wav";
+static const char s_boom_sound_one[]           = "../Resources/Music/boom.wav";
+static const char s_boom_sound_two[]           = "../Resources/Music/boom2.wav";
+static const char s_enemy_under_fire[]           = "../Resources/Music/enemyuderfire.wav";
 
 //tmx
 static const char s_level01[]           = "../Resources/level01.tmx";
